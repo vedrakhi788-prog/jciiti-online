91f1d2736f46496553b54f3bc146c0f23c116a5e
vedrakhi788-prog <vedrakhi788@gmail.com>
2026-02-04 12:43:03 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,8 +79,7 @@ if(form){
   bd.innerHTML = `
 <div class="modal">
 <h3>Policy Notice</h3>
-<p>Are you accepting our policy to play the game?</p>
-<p>Welcome to Brucebt</p>
+<p>Are you accepting our policy to enter the game?</p>
 <div style="display:flex;gap:10px;flex-wrap:wrap">
 <button class="btn" id="age-yes">Yes, Accept</button>
 <button class="btn ghost" id="age-no">Close</button>
